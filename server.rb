a3cc7bd542937eb3105e6b0a3b51f1d9bfaff560
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2011-03-25 18:04:55 -0400

    indentation cleanup

--- server.rb
+++ server.rb
@@ -3,32 +3,32 @@ module GoGoGo
   def self.new
     return App.new
   end
-	class App < Sinatra::Base
-		set :root, File.dirname(__FILE__)
-		set :public, File.dirname(__FILE__) + '/public'
-		set :views, File.dirname(__FILE__) + '/views'
-		set :app_file, __FILE__
-		set :static, true
+  class App < Sinatra::Base
+    set :root, File.dirname(__FILE__)
+    set :public, File.dirname(__FILE__) + '/public'
+    set :views, File.dirname(__FILE__) + '/views'
+    set :app_file, __FILE__
+    set :static, true
 
-		helpers do
-			def p_(page)
-				haml ('_'+page.to_s).intern, :layout => false
-			end
-		end
-    
+    helpers do
+      def p_(page)
+        haml ('_'+page.to_s).intern, :layout => false
+      end
+    end
+
     get '/' do
-			haml :home
-		end
-		
-		get '/awesome' do
+      haml :home
+    end
+
+    get '/awesome' do
       haml :awesome, :layout=>false
     end
-		
-		get '/*' do
-		  haml :home
+
+    get '/*' do
+      haml :home
     end
-    
 
 
-	end # ppA
+
+  end # ppA
 end #yesaerC
